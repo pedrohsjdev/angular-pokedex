edc3f5922a59201160fd00c8c739c1e21e4ceabe
Pedro Henrique <pedrohsj.dev@gmail.com>
2024-04-08 18:01:51 -0300

    feat: adiciona componente pokemon-list

---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrl: './pokemon-list.component.css'
+})
+export class PokemonListComponent {
+
+    pokemons = ['Bulbassaur', 'Ivysaur', 'Venousar', 'Charmander'];
+
+}

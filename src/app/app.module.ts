d38c3e3afbeb0580f570412c88b76f32c8636dbb
Pedro Henrique <pedrohsj.dev@gmail.com>
2024-04-08 18:03:16 -0300

    feat: atualiza modulo app e inclue componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
